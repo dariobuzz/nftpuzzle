8be138f8f1b3939f84237480e324c87d8cbdf305
BALMAS Dario (CNH) <WF626@cnh2.cnhgroup.cnh.com>
2025-03-09 23:46:27 +0100

    update img

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -15,10 +15,14 @@ export default function DefaultLayout({
 				{children}
 			</main>
 			<footer style={{ color: 'white' }} className="w-full flex flex-col items-center justify-center py-3">
-			<p className="text-center"> Progetto finanziato dall’Unione europea – Next Generation EU e inserire i 2 loghi che trovate in allegato  per come previsto dal bando di cui vi riporto di seguito il testo:
-			"dd. a garantire il rispetto degli obblighi in materia di comunicazione e informazione previsti dall’art. 34 del Regolamento (UE) 2021/241 indicando nella documentazione progettuale che il progetto è finanziato nell’ambito del PNRR, con esplicito riferimento al finanziamento da parte dell’Unione europea e all’iniziativa Next Generation EU (utilizzando la frase “finanziato dall’Unione europea – Next Generation EU”), riportando nella documentazione progettuale l’emblema dell’Unione europea e fornire un’adeguata diffusione e promozione del progetto, anche online, sia web che social, in linea con quanto previsto dalla Strategia di Comunicazione del PNRR</p>
-				
-				<p>Copyright © 2025 NFT Puzzle</p>
+			<div className="flex space-x-4">
+			<img src="/CUL.jpg" alt="CUL" style={{ width: '100px', height: '50px' }} />
+			<p>   </p>
+<img src="/FIN.jpg" alt="FIN" style={{ width: '100px', height: '40px' }} />
+
+				</div>
+			
+				<p>Copyright 2025 @ Artshares srl</p>
 				<Link href="/terms" color="primary" className="mt-2 text-sm text-white">
 					Terms and Conditions
 				</Link>
