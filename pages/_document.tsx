e6f570514fc1a5afbcad0ca9e69484844944a37d
BALMAS Dario (CNH) <WF626@cnh2.cnhgroup.cnh.com>
2025-05-05 23:59:25 +0200

    terms2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,42 +1,46 @@
-// pages/_document.tsx
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* 1) Configurazione Cookie Consent */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _iub = _iub || [];
-              _iub.csConfiguration = {
-                "siteId": 4011032,
-                "cookiePolicyId": 16252479,
-                "lang": "en-GB",
-                "storage": { "useSiteId": true }
-              };
-            `,
-          }}
-        />
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* 1) Configurazione Cookie Consent */}
+          <Script id="iubenda-cfg" strategy="beforeInteractive">
+            {`var _iub = _iub || [];
+_iub.csConfiguration = {
+  "siteId": 4011032,
+  "cookiePolicyId": 16252479,
+  "lang": "en-GB",
+  "storage": { "useSiteId": true }
+};`}
+          </Script>
 
-        {/* 2) Autoblocking */}
-        <script src="https://cs.iubenda.com/autoblocking/4011032.js"></script>
+          {/* 2) Autoblocking */}
+          <Script
+            src="https://cs.iubenda.com/autoblocking/4011032.js"
+            strategy="afterInteractive"
+          />
 
-        {/* 3) GPP stub */}
-        <script src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+          {/* 3) GPP stub */}
+          <Script
+            src="//cdn.iubenda.com/cs/gpp/stub.js"
+            strategy="afterInteractive"
+          />
 
-        {/* 4) Core Iubenda Consent */}
-        <script
-          src="//cdn.iubenda.com/cs/iubenda_cs.js"
-          charSet="UTF-8"
-          async
-        ></script>
-      </Head>
-      <body className="min-h-screen bg-background font-sans antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+          {/* 4) Core Iubenda Consent */}
+          <Script
+            src="//cdn.iubenda.com/cs/iubenda_cs.js"
+            strategy="afterInteractive"
+            charSet="UTF-8"
+          />
+        </Head>
+        <body className="min-h-screen bg-background font-sans antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
